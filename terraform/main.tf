901ac1fb667e6d4309a211d4040532bd333e0500
Jay Stockhausen <jay.stockhausen@gmail.com>
2024-09-28 15:49:05 -0600

    db security group

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,7 +25,34 @@ provider "aws" {
   }
 }
 
-resource "aws_db_instance" "default" {
+resource "aws_security_group" "stocks_db" {
+  name        = "stocks-db-sg"
+  description = "Security group for stocks RDS instance."
+
+  tags = {
+    Name = "postgres-rds-sg"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "stocks_db" {
+  security_group_id = aws_security_group.stocks_db.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 5432
+  ip_protocol = "tcp"
+  to_port     = 5432
+}
+
+resource "aws_vpc_security_group_egress_rule" "stocks_db" {
+  security_group_id = aws_security_group.stocks_db.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 0
+  ip_protocol = "-1"
+  to_port     = 0
+}
+
+resource "aws_db_instance" "stocks" {
   allocated_storage          = 20
   db_name                    = "stocks"
   engine                     = "postgres"
@@ -35,4 +62,12 @@ resource "aws_db_instance" "default" {
   password                   = var.stock_db_pw
   auto_minor_version_upgrade = false
   skip_final_snapshot        = true
+
+  vpc_security_group_ids = [
+    aws_security_group.stocks_db.id
+  ]
+
+  tags = {
+    Name = "stocks"
+  }
 }
